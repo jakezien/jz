7a60b51a7d319f341320c3f4864e46abaf783335
jakezien <jakezien@users.noreply.github.com>
2021-03-13 20:42:27 +0000

    deployed via Gatsby Publish Action 🎩 for 474309aa9634043b04ba7a6ea3d33caad5cf599e

---
+++ component---src-pages-404-js-94ae39527426a0fef255.js
@@ -0,0 +1,2 @@
+(self.webpackChunkjakezien=self.webpackChunkjakezien||[]).push([[883],{9616:function(e,t,a){"use strict";a.r(t);var n=a(7294),l=a(9011),u=a(6179);t.default=function(e){var t=e.data,a=e.location,i=t.site.siteMetadata.title;return n.createElement(l.Z,{location:a,title:i},n.createElement(u.Z,{title:"404: Not Found"}),n.createElement("h1",null,"Not Found"),n.createElement("p",null,"You just hit a route that doesn't exist... the sadness."))}}}]);
+//# sourceMappingURL=component---src-pages-404-js-94ae39527426a0fef255.js.map
